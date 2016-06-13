4fd212d2ebf8328ad6309dcc7e6d71ff18ea76bf
beth <zhangbaige@innobuddy.com>
2016-06-13 13:10:08 +0800

    完成基础框架搭建

--- README.md
+++ README.md
@@ -4,3 +4,40 @@
 1.如果有多张帧图片，用一个image标签去承载图片，定时改变image的src属性
 
 2.把所有动画关键帧绘制在一张图片里，把图片作为元素的background-image,定时改变元素的background-position属性
+
+###设计一个通用的帧动画库
+1.需求分析
+    1.支持图片预加载
+    2.支持2种动画播放方式，及自定义每帧动画
+    3.支持单组动画控制循环次数（可支持无限次）
+    4.支持一组动画库完成，继续完成下一组动画
+    5.支持每个动画完成后有等待时间
+    6.支持动画暂停和继续播放
+    7.支持动画完成后执行回调函数
+
+2.编程接口
+    1.loadImage(imglist)//与加载图片
+    2.changePosition(ele, position, imageUrl)//改变元素的background-position动画
+    3.changeSrc(ele, imglist)//通过改变image元素的src
+    4.enterFrame(callback)//每一帧动画执行的函数，相当于用户可以自定义每一帧动画的callback
+    5.repeat(times)//动画重复执行的次数，times为空时表示无限次
+    6.repeatForever()//无限重复上一次动画，相当于repeat（），更友好的一个接口
+    7.wait(time)//每个动画执行完后等待的时间
+    8.then(callback)//动画执行完成后的回调函数
+    9.start(interval)//动画开始执行，interval表示动画执行的间隔
+    10.pause()//动画暂停
+    11.restart()//动画从上一次暂停处重新执行
+    12.dispose()//释放资源
+
+3.调用方式
+    1.支持链式调用，我们期望动词的方式描述接口，调用方式如下：
+        var animation = require("animation");
+        var demoAnimation = animation().loadImage(images).changePosition(ele, positions).repeat(2).then(function(){
+            //动画执行完成后调用此函数
+        });
+        demoAnimation.start(80);
+
+4.代码设计
+    1.任务链
+    2.记录当前任务链
+    3.
---
+++ src/animation.js
@@ -0,0 +1,223 @@
+/**********************************************
+ * created by beth on 2016年6月13日
+ * 动画库
+ **********************************************/
+
+'use strict';
+
+var loadImage = require('./imageloader');
+
+//初始化状态
+var STATE_INITIAL = 0;
+//开始状态
+var STATE_START = 1;
+//停止状态
+var STATE_STOP = 2;
+
+//同步任务
+var TASK_SYNC = 0;
+//异步任务
+var TASK_ASYNC = 1;
+
+/**
+ * 帧动画类
+ */
+function Animation() {
+    this.taskQueue = [];
+    this.index = 0;
+
+    this.state = STATE_INITIAL;
+}
+
+/**
+ * 添加一个同步任务，去预加载图片
+ * @param  {[type]} imglist [description]
+ * @return {[type]}         [description]
+ */
+Animation.prototype.loadImage = function(imglist) {
+    var taskFn = function(next) {
+        loadImage(imglist.slice(), next);
+    }
+    var type = TASK_SYNC;
+
+    return this._add(taskFn, type);
+}
+
+/**
+ * 添加一个异步定时任务，通过定时改变图片背景位置，实现帧动画
+ * @param  {[type]} ele       dom对象
+ * @param  {[type]} positions [description]
+ * @param  {[type]} imageUrl  [description]
+ * @return {[type]}           [description]
+ */
+Animation.prototype.changePosition = function(ele, positions, imageUrl) {
+
+}
+
+/**
+ * 添加一个异步定时任务，通过定时改变image标签的src属性，实现帧动画
+ * @param  {[type]} ele     [description]
+ * @param  {[type]} imglist [description]
+ * @return {[type]}         [description]
+ */
+Animation.prototype.changeSrc = function(ele, imglist) {
+
+}
+
+/**
+ * 高级用法，添加一个异步定时执行的任务，该任务自定义动画每帧执行的任务函数
+ * @param  {[type]} taskFn [description]
+ * @return {[type]}        [description]
+ */
+Animation.prototype.enterFrame = function(taskFn) {
+
+}
+
+/**
+ * 添加一个同步任务，可以在上一个任务完成后执行回调函数
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+Animation.prototype.then = function(callback) {
+
+}
+
+/**
+ * 开始执行任务  异步任务执行的间隔
+ * @param  {[type]} interval [description]
+ * @return {[type]}          [description]
+ */
+Animation.prototype.start = function(interval) {
+    if (this.state === STATE_START) {
+        return this;
+    }
+
+    //如果任务链中没有任务，则返回
+    if (!this.taskQueue.length) {
+        return this;
+    }
+    this.state = STATE_START;
+    this.interval = interval;
+    this._runTask();
+    return this;
+}
+
+/**
+ * 添加一个同步任务，该任务就是回退到上一个任务，
+ * 实现重复上一个任务的效率，可以定义重复的次数
+ * @param  {[type]} times [description]
+ * @return {[type]}       [description]
+ */
+Animation.prototype.repeat = function(times) {
+
+}
+
+/**
+ * 添加一个同步任务，相当于repeat（）更友好的接口，无限循环上一次任务
+ * @return {[type]} [description]
+ */
+Animation.prototype.repeatForever = function() {
+
+}
+
+/**
+ * 设置当前任务执行结束后到下一个任务开始前的等待时间
+ * @param  {[type]} time [description]
+ * @return {[type]}      [description]
+ */
+Animation.prototype.wait = function(time) {
+
+}
+
+/**
+ * 暂停当前异步定时任务
+ * @return {[type]} [description]
+ */
+Animation.prototype.pause = function() {
+
+}
+
+/**
+ * 重新执行上一次暂停的异步任务
+ * @return {[type]} [description]
+ */
+Animation.prototype.restart = function() {
+
+}
+
+/**
+ * 释放资源
+ * @return {[type]} [description]
+ */
+Animation.prototype.dispose = function() {
+
+}
+
+/**
+ * 添加一个任务到任务队列中
+ * @param {[type]} taskFn 任务方法
+ * @param {[type]} type   任务类型
+ */
+Animation.prototype._add = function(taskFn, type) {
+    this.taskQueue.push({
+        taskFn: taskFn,
+        type: type
+    });
+
+    return this;
+}
+
+/**
+ * 执行任务
+ * @return {[type]} [description]
+ */
+Animation.prototype._runTask = function() {
+    if (!this.taskQueue || this.state !== STATE_START) {
+        return;
+    }
+    //任务执行完毕
+    if (this.index === this.taskQueue.length) {
+        this.dispose();
+        return;
+    }
+    //获得任务链上的当前任务
+    var task = this.taskQueue[this.index];
+    if (task.type === TASK_SYNC) {
+        this._syncTask(task);
+    } else {
+        this._asyncTask(task);
+    }
+}
+
+/**
+ * 同步任务
+ * @param  {[type]} task [description]
+ * @return {[type]}      [description]
+ */
+Animation.prototype._syncTask = function(task){
+	var me = this;
+	var next = function(){
+		//切换到下一个任务
+		me._next();
+	};
+	var taskFn = task.taskFn;
+	taskFn(next);
+}
+
+/**
+ * 异步任务
+ * @param  {[type]} task [description]
+ * @return {[type]}      [description]
+ */
+Animation.prototype._asyncTask = function(task){
+
+}
+
+/**
+ * 切换到下一个任务
+ * @return {[type]} [description]
+ */
+Animation.prototype._next = function(){
+	this.index++;
+	this._runTask();
+}
